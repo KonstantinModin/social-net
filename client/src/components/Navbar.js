5c7835b878ecc82ad8415557c3024694fe7883fe
KonstantinModin <konstantin.v.modin@gmail.com>
2020-04-01 06:56:45 +0200

    updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,58 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../redux/actions";
+import PropTypes from "prop-types";
 import logo from "../img/ps.jpg";
 
-const Navbar = () => {
+const Navbar = ({ logout, isAuth }) => {
+    const authLinks = (
+        <>
+            <li>
+                <NavLink to="/dashboard">Dashboard</NavLink>
+            </li>
+            <li>
+                <span onClick={logout}>
+                    <i className="fas fa-sign-out-alt"></i>
+                    <span className="hide-sm"> Logout</span>
+                </span>
+            </li>
+        </>
+    );
+
+    const guestLinks = (
+        <>
+            <li>
+                <NavLink to="/developers">Developers</NavLink>
+            </li>
+            <li>
+                <NavLink to="/register">Register</NavLink>
+            </li>
+            <li>
+                <NavLink to="/login">Login</NavLink>
+            </li>
+        </>
+    );
+
     return (
         <nav className="navbar bg-dark">
             <Link className="logo" to="/">
                 <img src={logo} alt="background"></img> <h1>PSN</h1>
             </Link>
-            <ul>
-                <li>
-                    <NavLink to="/developers">Developers</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register">Register</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">Login</NavLink>
-                </li>
-            </ul>
+            <ul>{isAuth ? authLinks : guestLinks}</ul>
         </nav>
     );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+    isAuth: PropTypes.bool,
+    logout: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({ auth: { isAuthenticated: isAuth } }) => {
+    return {
+        isAuth
+    };
+};
+
+export default connect(mapStateToProps, { logout })(Navbar);
